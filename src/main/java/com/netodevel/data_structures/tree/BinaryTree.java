c600c4e115bf35b6d6e3d59391e0fdde0cab8e40
NetoDevel <josevieira.dev@gmail.com>
2021-10-04 19:00:28 -0300

    BinaryTree Adds height function Adds post order traversal

--- src/main/java/com/netodevel/data_structures/tree/BinaryTree.java
+++ src/main/java/com/netodevel/data_structures/tree/BinaryTree.java
@@ -24,4 +24,40 @@ public class BinaryTree {
         }
         return this;
     }
+
+    public BinaryTree postOrderTraversal(Node node) {
+        if (node == null) {
+            node = this.root;
+        }
+
+        if (node.left != null) {
+            postOrderTraversal(node.left);
+        }
+        if (node.right != null) {
+            postOrderTraversal(node.right);
+        }
+
+        System.out.println(node);
+        return this;
+    }
+
+    public int height(Node node) {
+        if (node == null) {
+            node = this.root;
+        }
+        int hleft = 0;
+        int hright = 0;
+
+        if (node.left != null) {
+            hleft = height(node.left);
+        }
+        if (node.right != null) {
+            hright = height(node.right);
+        }
+        if (hright > hleft) {
+            return hright + 1;
+        }
+        return hleft + 1;
+    }
+
 }
--- src/main/java/com/netodevel/data_structures/tree/BinaryTreeMain.java
+++ src/main/java/com/netodevel/data_structures/tree/BinaryTreeMain.java
@@ -2,20 +2,18 @@ package com.netodevel.data_structures.tree;
 
 public class BinaryTreeMain {
 
-    //ler em ordem simetrica
-    //regra para inserir registros em uma arvore;
-
     public static void main(String[] args) {
-/*
-        var binaryTree = new BinaryTree(7);
-        binaryTree.root.left = new Node(18);
-        binaryTree.root.right = new Node(14);
-        System.out.println(binaryTree.root);
-*/
-
         var binaryTree = new BinaryTree();
         prepareDataToTree(binaryTree);
+
+        System.out.println("----> simetric traversal");
         binaryTree.simetricTraversal(null);
+
+        System.out.println("----> post order traversal");
+        binaryTree.postOrderTraversal(null);
+
+        System.out.println("----> height");
+        System.out.println(binaryTree.height(null));
     }
 
     private static void prepareDataToTree(BinaryTree binaryTree) {
